1458114669614d3c6b5c991db0d1a5e8f4f01521
Karolis Dailidonnis <mail@karolisdailidonis.de>
2022-02-08 11:47:32 +0100

    implement insert in querybuilder and DORMModel

--- src/DORM/Database/SQLClasses/Insert.php
+++ src/DORM/Database/SQLClasses/Insert.php
@@ -1,6 +1,35 @@
 <?php
 namespace DORM\Database\SQLClasses;
 
+
 class Insert {
+    
+    private $table;
+    
+    private $columns = [];
+
+    private $values = [];
+
+
+    public function __construct( string $table){
+        $this->table = $table;
+    }
+
+    public function columns( array $columns ): self {
+        $this->columns = $columns;
+        return $this;
+    }
+
+    public function values( array $values ): self {
+        $this->values = $values;
+        return $this;
+    }
+
+    public function __toString(): string {
+
+        return "INSERT INTO " . $this->table
+                . " ( " . implode(", ", $this->columns ) . " ) VALUES ( '" . implode("', '",     $this->values ) . "' )";
+    }
+
 
 }
