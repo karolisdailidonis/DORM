3638654ab1ea874bec8048da0eba536e199262e1
Sven Kirtz <sven@kirtz.eu>
2023-03-16 14:40:26 +0100

    Added Replace job for MSSQL

--- DORM/Database/SQLClasses/Replace.php
+++ DORM/Database/SQLClasses/Replace.php
@@ -12,6 +12,8 @@ class Replace
 
     private $values = [];
 
+    private $where = null;
+
     private $sqlType = null;
 
     public function __construct(string $table, string $sqlType)
@@ -32,6 +34,13 @@ class Replace
         return $this;
     }
 
+    // where is only required for some db types like mssql
+    public function where($var): self
+    {
+        $this->where = new Where($var);
+        return $this;
+    }
+
     public function __toString(): string
     {
         switch ($this->sqlType) {
@@ -41,13 +50,21 @@ class Replace
                 return "REPLACE INTO " . $this->table
                  . " ( " . implode(", ", $this->columns ) . " ) VALUES ( '" . implode("', '", $this->values ) . "' );";
 
-            // case 'mssql':
-            //     return "UPDATE " . $this->table
-            //        . " SET " . implode( ", ", $this->columns )
-            //        . ( $this->where === null  ?  " " : $this->where )
-            //        . "IF @@ROWCOUNT=0"
-            //        . "INSERT INTO " . $this->table
-            //        . " ( " . implode(", ", $this->columns ) . " ) VALUES ( '" . implode("', '", $this->values ) . "' );";
+            case 'mssql':
+                // create the string for the columns/value pairs for the update with the columns and values arrays
+                $keyvalues = '';
+                foreach ($this->columns as $key => $value) {
+                  $keyvalues .= $value . ' = \'' . $this->values[$key] . '\', ';
+                }
+                $keyvalues = rtrim($keyvalues, ', ');
+
+                // for mssql we use an update and if no new row was added, then use an insert
+                return "UPDATE " . $this->table
+                   . " SET " . $keyvalues
+                   . $this->where
+                   . "IF @@ROWCOUNT=0"
+                   . "INSERT INTO " . $this->table
+                   . " ( " . implode(", ", $this->columns ) . " ) VALUES ( '" . implode("', '", $this->values ) . "' );";
 
             default:
                 return "NO SQL TYPE";
